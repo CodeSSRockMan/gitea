1fb96fa7c0d49b00dcffd1a44ba7562cfbd736e0
Hernan3scobar <hernanescobarsanchez@gmail.com>
2025-05-05 00:52:12 -0400

    fixing double declarations

--- devops_CI_CD/terraform/modules/aws/vpc/main.tf
+++ devops_CI_CD/terraform/modules/aws/vpc/main.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 resource "aws_vpc" "main" {
   cidr_block           = var.cidr_block
   enable_dns_support   = true
@@ -47,16 +46,3 @@ resource "aws_internet_gateway" "igw" {
     Name = "main-igw"
   }
 }
-=======
-
-resource "aws_vpc" "gitea_main" {
-  cidr_block = var.cidr_block
-}
-
-resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.gitea_main.id
-  cidr_block        = cidrsubnet(var.cidr_block, 8, 0)
-  availability_zone = "us-east-1a"
-}
-
->>>>>>> origin/develop
