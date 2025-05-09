1d86d925abdd2f081f38aecbb055646af60500e1
Hernan3scobar <hernanescobarsanchez@gmail.com>
2025-05-08 21:25:46 -0400

    deploy: adding correct files and correct identation

--- devops_CI_CD/terraform/modules/gcp/firewall/main.tf
+++ devops_CI_CD/terraform/modules/gcp/firewall/main.tf
@@ -33,3 +33,19 @@ resource "google_compute_firewall" "allow_ssh_iap" {
 
   description   = "Allow SSH access through IAP only"
 }
+
+resource "google_compute_firewall" "allow_gitea_web" {
+  name    = "${var.gitea_vm_name}-fw-gitea"
+  network = var.network
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3000"]
+  }
+
+  direction     = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]  # o restringe si quieres más seguridad
+  target_tags   = var.target_tags
+
+  description   = "Allow external access to Gitea web interface on port 3000"
+}
